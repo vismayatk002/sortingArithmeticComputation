1bd948b1f98389ad9eaa435762642833409a1b69
vismayatk002 <vismayatk002@gmail.com>
2021-06-04 15:37:43 +0530

    added : addition and multiplication with a,b & c

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -2,3 +2,6 @@
 read -p "Enter first number :" a
 read -p "Enter second number :" b 
 read -p "Enter third number :" c
+echo "Entered Numbers :" $a "," $b "," $c
+result1=$(($a + ($b * c)))
+echo $result1
