1bffdce066047f03536b27154c50e7ca5378ec60
vismayatk002 <vismayatk002@gmail.com>
2021-06-04 16:59:51 +0530

    Updated :Stored results in an Array

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -8,4 +8,11 @@ compute['result1']=$(($a + ($b * $c)))
 compute['result2']=$((($a * $b) + $c))
 compute['result3']=$(($c + ($a / $b)))
 compute['result4']=$((($a % $b) + $c))
-echo ${compute[@]}
+echo "Results in Dictionary :" ${compute[@]}
+i=0
+for result in "${!compute[@]}"
+do
+	computeArray[$i]=${compute[$result]}
+	((i++))
+done
+echo "Results in Array :" ${computeArray[@]}
