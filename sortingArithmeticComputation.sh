b920d5634b8f8de1167c466cfec08bb65cfa3f72
vismayatk002 <vismayatk002@gmail.com>
2021-06-04 16:45:47 +0530

    added $ sign

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -4,8 +4,8 @@ read -p "Enter first number :" a
 read -p "Enter second number :" b 
 read -p "Enter third number :" c
 echo "Entered Numbers :" $a "," $b "," $c
-compute['result1']=$(($a + ($b * c)))
-compute['result2']=$((($a * $b) + c))
-compute['result3']=$((c + (a / b)))
-compute['result4']=$(((a % b) + c))
+compute['result1']=$(($a + ($b * $c)))
+compute['result2']=$((($a * $b) + $c))
+compute['result3']=$(($c + ($a / $b)))
+compute['result4']=$((($a % $b) + $c))
 echo ${compute[@]}
